9c0b29a26191a8f639391bf85112550bf0edc6e6
Martez Reed <martez.reed@greenreedtech.com>
2026-06-03 19:43:30 -0500

    Fix mkimge

--- scripts/mkimg.myappliance.sh
+++ scripts/mkimg.myappliance.sh
@@ -1,23 +1,7 @@
 profile_myappliance() {
     profile_standard
 
-    title="My Appliance"
-
-    apks="
-        alpine-base
-        linux-lts
-        linux-firmware
-        openssh
-        curl
-        jq
-        demo
-    "
-
-    repositories="
-        http://dl-cdn.alpinelinux.org/alpine/v3.23/main
-        http://dl-cdn.alpinelinux.org/alpine/v3.23/community
-        file://repo/packages
-    "
+    apks="$apks alpine-base linux-lts openssh curl"
 
     kernel_cmdline="quiet"
 }
